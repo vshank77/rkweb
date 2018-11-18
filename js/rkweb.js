c9ba7b7b69e475a7c4be9b07edb7a94320aa5807
Shankar Vasudevan <shankar@steel-eye.com>
2018-11-18 22:59:58 +0000

    updated images

--- js/rkweb.js
+++ js/rkweb.js
@@ -61,4 +61,20 @@
     $.magnificPopup.close();
   });
 
+  $(document).ready(function() {
+    $('.popup-gallery').magnificPopup({
+      delegate: 'a',
+      type: 'image',
+      tLoading: 'Loading image #%curr%...',
+      mainClass: 'mfp-img-mobile',
+      gallery: {
+        enabled: true,
+        navigateByImgClick: true,
+        preload: [0,1]
+      },
+      image: {
+        tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+      }
+    });
+});
 })(jQuery); // End of use strict
