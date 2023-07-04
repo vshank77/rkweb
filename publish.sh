9b3240ddcea8e2f3213fa9b09c0a1cd034bae434
Shankar Vasudevan <shankar@clippd.io>
2023-07-04 20:53:55 +0100

    fixed css

--- publish.sh
+++ publish.sh
@@ -4,7 +4,7 @@ DEFAULT="vshank77"
 PROFILE=${AWS_PROFILE:-$DEFAULT}
 BUCKET=radhakalyanam.uk
 DIR=_site/
-aws  s3  sync $DIR s3://$BUCKET/
+aws  s3  sync $DIR s3://$BUCKET/ --delete
 aws cloudfront create-invalidation \
     --distribution-id EZ57PG7SAFHY2 \
     --paths "/css/**" "/js/**" "/img/**" "/index.html" "/sponsor.html" "/yester.html" "/**"
